fd9276f43a26550ed939cda62da382f9663a8424
patrixia-mac <patywilthew@gmail.com>
2017-06-19 21:50:14 -0400

    Some PEP conventions

--- Port-Checker/portchecker-with-netstat.py
+++ Port-Checker/portchecker-with-netstat.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
-"""
-Usage: sudo ./portchecker-with-netstat.py
+"""Usage: sudo ./portchecker-with-netstat.py
 
 The purpose of this script is to determine if there are any unexpected 
 running applications that have ports open for listening. Its goal is to
-detect if the machine has been compromised.
-"""
+detect if the machine has been compromised."""
 import socket
 import subprocess
 import re
@@ -13,287 +11,266 @@ import syslog
 #
 # Update FILE_NAME with the name of the csv file containing every 
 # expected:
-# ProcessName, 
+# Process Name, 
 # Port, 
 # Protocol,
 # Owner, 
-# ShouldListenToPublicIPs=y/n, 
-# ShouldListenToLocalHost=y/n.
+# Should Listen To Public IPs = y/n, 
+# Should Listen To Localhost = y/n.
 #
-# Each Line of this file should have the following format: 
+# Each line of this file should have the following format: 
 #
-#	Port,TCP/UDP,ProcessName,Owner,Public?,Localhost?
+#   port,TCP/UDP,process_name,owner,public,localhost
 #
 # Example:
 #
-#	80,TCP,apache,root,y,y
+#   80,TCP,apache,root,y,y
 #
 FILE_NAME = "netports.csv"
 
-# The following dictionaries get their keys and values from
-# FILE_NAME
+# The following dictionaries get their keys and values from FILE_NAME
 proc_users_dic = {}
 proc_port_prot_dic = {}
 proc_ip_dic = {}
 
 # The following block creates a dictionary of 
-# {ProcessName: Owner} 
-FileObject = open(FILE_NAME, "r")
+# {process_name: owner} 
+file_object = open(FILE_NAME, "r")
 
-for Line in FileObject.readlines():
-        ProcessName = Line.split(",")[2]
-        Owner = Line.split(",")[3]
+for ln in file_object.readlines():
+    process_name = ln.split(",")[2]
+    owner = ln.split(",")[3]
 
-        Pair = {ProcessName: (Owner)}
+    pair = {process_name: (owner,)}
 
-        if proc_users_dic.has_key(ProcessName):
-                if (Owner not in proc_users_dic[ProcessName]):
-                        Temp = proc_users_dic[ProcessName]
-                        Temp = Temp + (Owner)
-                        proc_users_dic[ProcessName] = Temp
-        else:
-                proc_users_dic.update(Pair)
+    if process_name in proc_users_dic:
+        if owner not in proc_users_dic[process_name]:
+            tmp = proc_users_dic[process_name]
+            tmp = tmp + (owner,)
+            proc_users_dic[process_name] = tmp
+    else:
+            proc_users_dic.update(pair)
 
 # The following block creates a dictionary of 
-# {ProcessName: [(Port,Protocol)]} 
-FileObject = open(FILE_NAME, "r")
+# {process_name: [(port,protocol)]} 
+file_object = open(FILE_NAME, "r")
 
-for Line in FileObject.readlines():
-        ProcessName = Line.split(",")[2]
-        Port = Line.split(",")[0]
-        Protocol = Line.split(",")[1]
+for ln in file_object.readlines():
+    port, protocol, process_name = ln.split(",")[:3]
 
-        PortTuple = (Port,Protocol)
-        Pair = {ProcessName: [PortTuple]}
+    port_tuple = (port,protocol)
+    pair = {process_name: [port_tuple]}
 
-        if proc_port_prot_dic.has_key(ProcessName):
-		if (PortTuple not in proc_port_prot_dic[ProcessName]):
-                        proc_port_prot_dic[ProcessName].append(PortTuple)
-        else:
-                proc_port_prot_dic.update(Pair)
+    if process_name in proc_port_prot_dic:
+        if port_tuple not in proc_port_prot_dic[process_name]:
+            proc_port_prot_dic[process_name].append(port_tuple)
+    else:
+        proc_port_prot_dic.update(pair)
 
 # The following block creates a dictionary of
-# {ProcessName: (Public,Localhost)}
-FileObject = open(FILE_NAME, "r")
+# {process_name: (public,localhost)}
+file_object = open(FILE_NAME, "r")
 
-for Line in FileObject.readlines():
-	ProcessName = Line.split(",")[2]
-	Public = Line.split(",")[4]
-	Localhost = Line.split(",")[5]
-	Localhost = Localhost.strip('\r\n')
+for ln in file_object.readlines():
+    process_name = ln.split(",")[2]
+    public = ln.split(",")[4]
+    localhost = ln.split(",")[5]
+    localhost = localhost.strip('\r\n')
 
-	PublicLocal = (Public,Localhost)
-	Pair = {ProcessName: [PublicLocal]}
+    public_local_tuple = (public,localhost)
+    pair = {process_name: [public_local_tuple]}
 
-	if proc_ip_dic.has_key(ProcessName):
-		if PublicLocal not in proc_ip_dic[ProcessName]:
-			proc_ip_dic[ProcessName].append(PublicLocal)
-	else:
-		proc_ip_dic.update(Pair)
+    if process_name in proc_ip_dic:
+        if public_local_tuple not in proc_ip_dic[process_name]:
+            proc_ip_dic[process_name].append(public_local_tuple)
+    else:
+        proc_ip_dic.update(pair)
 
-FileObject.close()
+file_object.close()
 
 
 def run_netstat():
-	"""Returns the output of 'netstat -plunt'.
-	"""
-
-	return subprocess.Popen(['netstat', '-plunt'], \
-	       stdout=subprocess.PIPE).communicate()[0]
+    """Returns output of 'netstat -plunt'."""
+    return subprocess.Popen(['netstat', '-plunt'], \
+                            stdout=subprocess.PIPE).communicate()[0]
 
 
-def into_list_of_lists(StringInput):
-	"""Receives a multiLine string and returns a list of lines excluding
-	the first two.
-	"""
-
-	return [re.split("\s+", Line) for Line in StringInput.splitlines()[2:]]
+def into_list_of_lists(input_string):
+    """Receives a multiline string and returns a list of lines excluding the
+    first two (as the first two lines of the netstat output are not used)."""
+    return [re.split("\s+", ln) for ln in input_string.splitlines()[2:]]
 
 
-def format_list(List):
-	"""Receives a list of lines, each corresponding to a line of 
-	"netstat -plunt" output and returns a list of processes (where each 
-	process is a list).
-	"""
+def format_list(input_list):
+    """Receives a list of lines, each corresponding to a line of the
+    "netstat -plunt" output, and returns a list of processes (where each 
+    process is represented as a list)."""
+    list_ = input_list
 
-	for Line in List:
-		Line.remove(Line[1])
-		Line.remove(Line[1])
+    for ln in list_:
+        ln.remove(ln[1])
+        ln.remove(ln[1])
 
-		if len(Line)>5:
-			Line.remove(Line[3]) # Removes the word LISTEN
+        if len(ln)>5:
+            ln.remove(ln[3]) # Removes the word LISTEN
 
-		if len(Line)==6:
-			Line.remove(Line[4])	# Removes the word master, 
-						# i.e. "nginx: master"
-		if (Line[3])[-1]==":":
-			Line[3] = (Line[3])[:-1] # Removes the ":" from "nginx:"
+        if len(ln)==6:
+            ln.remove(ln[4])    # Removes the word master, 
+                        # i.e. "nginx: master"
+        if (ln[3])[-1]==":":
+            ln[3] = (ln[3])[:-1] # Removes the ":" from "nginx:"
 
-		del Line[4]  # Removes an empty string from the list
+        del ln[4]  # Removes an empty string from the list
 
-		ipv4 = Line[1].count(':')==1  # Checks if address is ipv4 or v6
+        ipv4 = ln[1].count(':')==1  # Checks if address is ipv4 or v6
 
-		# Adds port number at the end of the list and removes it from the local address
-		# for both ipv4 and ipv6 cases
-		if ipv4:
-			Line.append(Line[1].split(":")[1]) 
-			Line[1] = Line[1].split(":")[0]
-	
-		else:
-			Line.append(''.join(Line[1].split(":")[-1:]))
-			Line[1] = ''.join(re.split('(\:)',Line[1])[:-1])
-			Line[0] = Line[0][:-1]
+        # Adds port number at the end of the list and removes it from the local
+        # address for both ipv4 and ipv6 cases
+        if ipv4:
+            ln.append(ln[1].split(":")[1]) 
+            ln[1] = ln[1].split(":")[0]
+    
+        else:
+            ln.append(''.join(ln[1].split(":")[-1:]))
+            ln[1] = ''.join(re.split('(\:)',ln[1])[:-1])
+            ln[0] = ln[0][:-1]
 
-	# Adds respective owners to processes
-	List = add_owners(List)
+    # Adds respective owners to processes
+    list_ = add_owners(list_)
 
-	return List
+    return list_
 
 
-def compare_port(Process):
-	"""Returns true if the process is using a Port/Protocol
-	specified in the proc_port_prot_dic dictionary.
-	"""
+def compare_port(process):
+    """Returns True if the process is using a port/protocol
+    specified in the proc_port_prot_dic dictionary."""
+    process_name = process[3]
+    port = process[4]
+    protocol = process[0].upper()
 
-	ProcessName = Process[3]
-	Port = Process[4]
-	Protocol = Process[0].upper()
-
-	if ProcessName not in proc_port_prot_dic:
-		return False
+    if process_name not in proc_port_prot_dic:
+        return False
  
-	TuplesList = proc_port_prot_dic[ProcessName]
-	PortTuple = (Port, Protocol)
+    tuples_list = proc_port_prot_dic[process_name]
+    port_tuple = (port, protocol)
 
-	if PortTuple in TuplesList:
-		return True
+    if port_tuple in tuples_list:
+        return True
 
-	# Cases when a port is not predictable but it is specified as 
-	# "highport" in FILE_NAME. Example: highport,UDP,rsyslogd,root,y,y
-	if len(Port)>=4:
-		PortTuple = ("highport",Protocol)
-		if PortTuple in TuplesList:
-			return True
-	
-	return False
-
-
-def compare_owner(Process):
-	"""Returns true if the process is owned by the user specified in the 
-	proc_users_dic dictionary.
-	"""
-
-	ProcessName = Process[3]
-	Owner = Process[5]
-
-	if ProcessName not in proc_users_dic:
-		return False
-
-	TuplesList = proc_users_dic[ProcessName]
-
-	if Owner in TuplesList:
-		return True
-	
-	return False
+    # Cases when a port is not predictable but it is specified as 
+    # "highport" in FILE_NAME. Example: highport,UDP,rsyslogd,root,y,y
+    if len(port)>=4:
+        port_tuple = ("highport",protocol)
+        if port_tuple in tuples_list:
+            return True
+    
+    return False
 
 
-def add_owners(ProcessesList):
-	"""Receives a list of processes and appends the process owner to each
-	of the lists.
-	"""
+def compare_owner(process):
+    """Returns True if the process is owned by the user specified in the 
+    proc_users_dic dictionary."""
+    process_name = process[3]
+    owner = process[5]
 
-	for Process in ProcessesList:
-		ProcessName = Process[3]
-		Pid = str(ProcessName.split("/")[0])
-		Process[3] = (ProcessName.split("/"))[1] # Removes PID from the process name
-		Arg = [Pid]
-		Ps = subprocess.Popen(['ps', '-up'] + Arg, \
-		     stdout=subprocess.PIPE).communicate()[0]	# Gets owner of that PID
-		Owner = (Ps.split("\n")[1]).split(" ")[0]
-		Process.append(Owner)
+    if process_name not in proc_users_dic:
+        return False
 
-	return ProcessesList
+    tuples_list = proc_users_dic[process_name]
+
+    if owner in tuples_list:
+        return True
+    
+    return False
 
 
-def compare_ip(Process):
-	"""Returns true if the process values of "Public?,Localhost?" are the
-	same as the corresponding values for the process in proc_ip_dic.
-	"""
+def add_owners(processes_list):
+    """Receives a list of processes and appends the process owner to each
+    of the lists."""
+    for process in processes_list:
+        process_name = process[3]
+        pid = str(process_name.split("/")[0])
+        process[3] = (process_name.split("/"))[1] 
+        arg = [pid]
+        ps = subprocess.Popen(['ps', '-up'] + arg,
+                              stdout=subprocess.PIPE).communicate()[0] 
+        owner = (ps.split("\n")[1]).split(" ")[0]
+        process.append(owner)
 
-	ProcessName = Process[3]
-	LocalAddress = Process[1]
+    return processes_list
 
-	PublicIPsListening = "y"	
-	LocalhostListening = "n"
-	
-	if LocalAddress == "127.0.0.1" or LocalAddress == "::1:":
-		PublicIPsListening = "n"
 
-	if LocalAddress == "127.0.0.1" or LocalAddress == "::1:" or \
-	   LocalAddress == "0.0.0.0" or LocalAddress == ":::":
-		LocalhostListening = "y"
+def compare_ip(process):
+    """Returns True if the process values of "public,localhost" are the
+    same as the corresponding values for the process in proc_ip_dic."""
+    process_name = process[3]
+    local_address = process[1]
 
-	PublicLocalTuple = (PublicIPsListening, LocalhostListening)
+    public_ips_listening = "y"    
+    localhost_listening = "n"
+    
+    if local_address == "127.0.0.1" or local_address == "::1:":
+       public_ips_listening = "n"
 
-	if PublicLocalTuple in proc_ip_dic[ProcessName]:
-		return True
+    if local_address == "127.0.0.1" or local_address == "::1:" or \
+       local_address == "0.0.0.0" or local_address == ":::":
+       localhost_listening = "y"
 
-	return False
+    public_local_tuple = (public_ips_listening, localhost_listening)
+
+    if public_local_tuple in proc_ip_dic[process_name]:
+        return True
+
+    return False
 
 
 def main():
-	"""Checks if all TCP/UDP processes are listening through the expected ports, 
-	are owned by the expected users, and are accepting connections from the 
-	expected IP addresses.
-	"""
+    """Checks if all TCP/UDP processes are listening through the expected ports, 
+    are owned by the expected users, and are accepting connections from the 
+    expected IP addresses."""
+    netstat = run_netstat()
 
-	Netstat = run_netstat()
+    # Creates a list of lists
+    processes_list = into_list_of_lists(netstat)
 
-	# Creates a list of lists
-	ProcessesList = into_list_of_lists(Netstat)
+    # Removes extra info and adds owners to each process
+    processes_list = format_list(processes_list)
 
-        # Removes extra info and adds owners to each process
-        ProcessesList = format_list(ProcessesList)
+    # At this point, a list within processes_list would look like:
+    # ['TCP', '0.0.0.0', '0.0.0.0:*', 'mysqld', '3306', 'mysql']
+    # where position 3 corresponds to the process name and 
+    # position 5 to its owner
 
-	# At this point, a list within ProcessesList would look like:
-	# ['TCP', '0.0.0.0', '0.0.0.0:*', 'mysqld', '3306', 'mysql']
-	# where position 3 corresponds to the process name and 
-	# position 5 to its owner
+    for process in processes_list:
+        protocol, local_address, _, process_name, port, owner = process[:5]
 
-	for Process in ProcessesList:
-		Protocol, LocalAddress, ProcessName, Port, Owner = \
-		Process[0], Process[1], Process[3], Process[4], Process[5]
+        correct_port, correct_owner, correct_ip = False, False, False
 
-		CorrectPort, CorrectOwner, CorrectIP = False, False, False
+        if compare_port(process):
+            correct_port = True
 
-		if compare_port(Process):
-			CorrectPort = True
+        if compare_owner(process):
+            correct_owner = True
 
-		if compare_owner(Process):
-			CorrectOwner = True
+        if compare_ip(process):
+            correct_ip = True
 
-		if compare_ip(Process):
-			CorrectIP = True
+        if not (correct_port and correct_owner and correct_ip):
+            message = "ALERT: "
+            should_log = True
+        else:
+            message = ""
+            should_log = False
 
-		if not (CorrectPort and CorrectOwner and CorrectIP):
-			Message = "ALERT: "
-			ShouldLog = True
-		else:
-			Message = ""
-			ShouldLog = False
-
-		Message += "Process " + ProcessName \
-		       + " is using " + Protocol \
-				+ " " + Port \
-		     + " with owner " + Owner \
-    + ", accepting connections from " + LocalAddress
-		
-		#print Message
-	
-		if ShouldLog:
-			syslog.syslog(syslog.LOG_ALERT, Message)
+        message += "process " + process_name \
+               + " is using " + protocol \
+                        + " " + port \
+             + " with owner " + owner \
+             + ", accepting connections from " + local_address
+    
+        if should_log:
+            syslog.syslog(syslog.LOG_ALERT, message)
 
 
 if __name__ == "__main__":
-	main()
-
+    main()
