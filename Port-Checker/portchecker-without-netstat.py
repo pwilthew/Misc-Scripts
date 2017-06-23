e66f9f8cd83dea7e2040c979a11517fb3d938e7b
pwilthew <patywilthew@gmail.com>
2017-06-23 12:46:03 -0400

    Fully functional

--- Port-Checker/portchecker-without-netstat.py
+++ Port-Checker/portchecker-without-netstat.py
@@ -18,7 +18,7 @@ import os
 # Process Name, Port, Protocol, Owner, Should listen to public IPs = y/n, 
 # Should listen to localhost = y/n.
 #
-# Each line of this file should have the following format: 
+# Each line of FILE_NAME should have the following format: 
 #
 #   port,TCP/UDP,process_name,owner,public,localhost
 #
@@ -26,9 +26,11 @@ import os
 #
 #   80,TCP,apache,root,y,y
 #
-FILE_NAME = "netports.csv"
-pids = filter(lambda x: x.isdigit() and os.path.isdir("/proc/"+x),
-              os.listdir("/proc/"))
+FILE_NAME = 'netports.csv'
+
+# pids will contain a list of the pid directories under /proc/
+pids = filter(lambda x: x.isdigit() and os.path.isdir('/proc/'+x),
+              os.listdir('/proc/'))
 
 # The following dictionaries get their keys and values from FILE_NAME
 proc_users_dic = {}
@@ -38,10 +40,10 @@ proc_ip_dic = {}
 # The following block creates a dictionary of 
 # {process_name: (owners)}
 # i.e.: 'nginx': ('nginx', 'root')
-file_object = open(FILE_NAME, "r")
+file_object = open(FILE_NAME, 'r')
 
 for ln in file_object.readlines():
-    _, _, process_name, owner = ln.split(",")[:4]
+    _, _, process_name, owner = ln.split(',')[:4]
 
     pair = {process_name: (owner,)}
 
@@ -56,10 +58,10 @@ for ln in file_object.readlines():
 # The following block creates a dictionary of 
 # {process_name: [(port,protocol)]}
 # i.e.: 'nginx': [('80', 'TCP'), ('443', 'TCP')]
-file_object = open(FILE_NAME, "r")
+file_object = open(FILE_NAME, 'r')
 
 for ln in file_object.readlines():
-    port, protocol, process_name = ln.split(",")[:3]
+    port, protocol, process_name = ln.split(',')[:3]
 
     port_tuple = (port,protocol)
     pair = {process_name: [port_tuple]}
@@ -73,10 +75,10 @@ for ln in file_object.readlines():
 # The following block creates a dictionary of
 # {process_name: (public,localhost)}
 # i.e.: 'named': [('y', 'y'), ('n', 'y')]
-file_object = open(FILE_NAME, "r")
+file_object = open(FILE_NAME, 'r')
 
 for ln in file_object.readlines():
-    _, _, process_name, _, public, localhost = ln.split(",")[:6]
+    _, _, process_name, _, public, localhost = ln.split(',')[:6]
 
     localhost = localhost.strip('\r\n')
 
@@ -99,7 +101,7 @@ def get_sockets(protocol):
         print "Error: get_sockets() input can only be 'tcp' or 'udp'"
         exit()
 
-    arg = ['/proc/net/' + protocol]  # Argument needed in next line
+    arg = ['/proc/net/' + protocol]
 
     output = subprocess.Popen(['cat'] + arg, 
                             stdout=subprocess.PIPE).communicate()[0]
@@ -110,7 +112,7 @@ def get_sockets(protocol):
     protocol = protocol.upper()
 
     if '6' in protocol:
-        protocol = protocol[:-1]
+        protocol = protocol[:-1]  # Removes the 6 from the protocol
 
     for ln in list_of_lines: # From list of lines to list of lists
         sub_list = [protocol,] 
@@ -131,7 +133,7 @@ def format_list(input_list):
         while '' in sub_list:
             sub_list.remove('')
 
-        if '06' in sub_list[4]:
+        if ('0A' not in sub_list[4]) and ('07' not in sub_list[4]):
             continue
 
         sub_list.remove(sub_list[1]) # Removes sl (number of line)
@@ -142,19 +144,20 @@ def format_list(input_list):
         sub_list.remove(sub_list[3]) # Removes state
 
         # Save port for later appending to sub_list
-        port = int(sub_list[1].split(":")[1], 16)
+        port = int(sub_list[1].split(':')[1], 16)
 
         # Translate ip addresses sub_list[1] and sub_list[2] 
-        ip = sub_list[1].split(":")[0]
+        ip = sub_list[1].split(':')[0]
         sub_list[1] = translate_ip(ip)
 
-        ip = sub_list[2].split(":")[0]
+        ip = sub_list[2].split(':')[0]
         sub_list[2] = translate_ip(ip)
 
-
+        # Values of uid and inode needed for next function calls
         uid = sub_list[3]
         inode = sub_list[4]
 
+        # Completing sub_list with process name, port, and owner username
         sub_list[3] = get_processname(inode)
         sub_list[4] = str(port)
         sub_list[5] = get_username(uid)
@@ -167,38 +170,40 @@ def format_list(input_list):
 
 
 def translate_ip(ip):
-    """ """
+    """Returns the standard decimal format of ip"""
     if len(ip) > 8:
         ip = base64.b16decode(ip)
         return socket.inet_ntop(socket.AF_INET6, 
                                 struct.pack('<4I', *struct.unpack('>4I', ip)))
 
     else:
-        return socket.inet_ntoa(struct.pack("<L", int(ip, 16)))
+        return socket.inet_ntoa(struct.pack('<L', int(ip, 16)))
 
 
 def get_username(uid):
-    """ """
+    """Returns the username of a user given its uid"""
     arg = ['cat', '/etc/passwd']
 
     passwd = subprocess.Popen(arg, stdout=subprocess.PIPE).communicate()[0]
 
     for ln in passwd.splitlines():
-        ln_list = ln.split(":")
+        ln_list = ln.split(':')
         if uid in ln_list[2]:
             return ln_list[0]
-    return "-"
+
+    return '-'
 
 
 def get_processname(inode):
-    """" """
+    """"Returns the name of a process given a socket inode"""
     found = False
 
     for pid in pids:
-        path = "/proc/" + pid + "/fd"
+        path = '/proc/' + pid + '/fd'
         arg = ['ls', '-l', path]
         file_descriptors = subprocess.Popen(arg, 
-                           stdout=subprocess.PIPE).communicate()[0]
+                           stdout=subprocess.PIPE,
+                           stderr=None).communicate()[0]
 
         # Check if a socket is using the inode
         if 'socket:['+inode+']' in file_descriptors:
@@ -207,13 +212,13 @@ def get_processname(inode):
 
     if found:
         # Get process name from first line of /proc/<pid>/status
-        path = '/proc/' + pid + "/status"
+        path = '/proc/' + pid + '/status'
         arg = ['cat', path]
         status = subprocess.Popen(arg, stdout=subprocess.PIPE).communicate()[0]
         status = status.splitlines()
-        return (status[0].split("\t"))[-1]
+        return ((status[0].split('\t'))[-1]).strip()
     else:
-        return "process-name"
+        return 'process-name'
 
 
 def compare_port(process):
@@ -235,7 +240,7 @@ def compare_port(process):
     # Cases when a port is not predictable but it is specified as 
     # "highport" in FILE_NAME. Example: highport,UDP,rsyslogd,root,y,y
     if len(port)>=4:
-        port_tuple = ("highport",protocol)
+        port_tuple = ('highport',protocol)
 
         if port_tuple in tuples_list:
             return True
@@ -269,15 +274,15 @@ def compare_ip(process):
     if process_name not in proc_ip_dic:
         return False
 
-    public_ips_listening = "y"    
-    localhost_listening = "n"
+    public_ips_listening = 'y'    
+    localhost_listening = 'n'
     
-    if local_address == "127.0.0.1" or local_address == "::1:":
-       public_ips_listening = "n"
+    if local_address == '127.0.0.1' or local_address == '::1':
+       public_ips_listening = 'n'
 
-    if local_address == "127.0.0.1" or local_address == "::1:" or \
-       local_address == "0.0.0.0" or local_address == ":::":
-       localhost_listening = "y"
+    if local_address == '127.0.0.1' or local_address == '::1' or \
+       local_address == '0.0.0.0' or local_address == '::':
+       localhost_listening = 'y'
 
     public_local_tuple = (public_ips_listening, localhost_listening)
 
@@ -318,24 +323,22 @@ def main():
             correct_ip = True
 
         if not (correct_port and correct_owner and correct_ip):
-            message = "ALERT: "
+            message = 'ALERT: '
             should_log = True
         else:
-            message = ""
+            message = ''
             should_log = False
 
-        message += "process " + process_name \
-               + " is using " + protocol \
-                        + " " + port \
-             + " with owner " + owner \
-        + ", listening from " + local_address
+        message += 'process ' + process_name \
+               + ' is using ' + protocol \
+                        + ' ' + port \
+             + ' with owner ' + owner \
+        + ', listening from ' + local_address
     
         print message
         
         if should_log:
             syslog.syslog(syslog.LOG_ALERT, message)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-
