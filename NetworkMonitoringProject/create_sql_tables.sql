12b65a867eb9c2413a72e8543d65c36e6489d09e
pwilthew <patywilthew@gmail.com>
2017-07-10 17:53:44 -0400

    added sql script

---
+++ NetworkMonitoringProject/create_sql_tables.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS devices, found_devices;
+
+CREATE TABLE devices (
+                      if_index INT(5) NOT NULL KEY,
+                      mac VARCHAR(50), 
+                      name VARCHAR(120), 
+                      management_ip VARCHAR(50), 
+                      up_link_ports VARCHAR(50));
+
+CREATE TABLE found_devices (
+                            if_index INT(5) NOT NULL KEY,
+                            mac VARCHAR(50), 
+                            vlan VARCHAR(5),
+                            staff_name VARCHAR(120),
+                            switch_port INT(5), 
+                            make_model VARCHAR(120),
+                            description VARCHAR(120),
+                            first_detection DATE,
+                            most_recent_detection DATE,
+                            allowed_vlan_list VARCHAR(120),
+                            most_recent_ipv4 VARCHAR(50),
+                            most_recent_ipv6 VARCHAR(50));
+
+
